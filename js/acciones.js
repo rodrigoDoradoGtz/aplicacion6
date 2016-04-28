87cfb9ccaabd2b043f6660c6aeb5ee4ae25df800
rodrigoDoradoGtz <rodrigodgtz3f.desaupoo@gmail.com>
2016-04-28 14:16:37 -0500

    1rasubida

---
+++ js/acciones.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    document.addEventListener('deviceready', function(){
+		$('#btnvibrar').on('tap', function (){
+			navigator.notification.vibrate(2000);
+		});
+	});
+});
