7242c92149171b8e98ce03d2710723419ede45dd
rodrigoDoradoGtz <rodrigodgtz3f.desaupoo@gmail.com>
2016-05-12 13:19:46 -0500

    Update acciones.js

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,9 @@
 // JavaScript Document
 $(document).ready(function(e) {
     document.addEventListener('deviceready', function(){
+    	$('#beep').on('tap', function (){
+			navigator.notification.beep(4);
+		});
 		$('#btnvibrar').on('tap', function (){
 			navigator.notification.vibrate(1250);
 		});
